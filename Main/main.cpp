0d60f7e45b6f7ca1cb5b53eb1390f14b88e8ad2e
wangsz <wangsizhan1994@163.com>
2026-03-17 16:23:54 +0800

    feat: initial commit — SCDAT project with auto-glob CMakeLists

---
+++ Main/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file main.cpp
+ * @brief Spacecraft Charging and Discharging Analysis Toolkit主程序入口
+ *
+ * @author Wang Sizhan
+ * @date 2026年3月17日
+ * @version 0.0.1
+ */
+
+#include <iostream>
+
+// ============================================================================
+// 主函数
+// ============================================================================
+
+/**
+ * @brief 主函数
+ */
+int main(int argc, char* argv[])
+{
+    return 0;
+}
---
+++ Main/main_PICcore.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file main.cpp
+ * @brief Spacecraft Charging and Discharging Analysis Toolkit主程序入口，本主函数调用项目中的所有PIC-MCC相关计算函数，计算等离子体典型算例，实现对PIC核心流程的校验
+ *
+ * @author Wang Sizhan
+ * @date 2026年3月17日
+ * @version 0.0.1
+ */
+
+#include <iostream>
+
+// ============================================================================
+// 主函数
+// ============================================================================
+
+/**
+ * @brief 主函数
+ */
+int main(int argc, char* argv[])
+{
+    return 0;
+}
